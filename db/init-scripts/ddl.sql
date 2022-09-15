e8c2731d49b6b4531be5d05af466f25e01d6fefe
Lucas Pires de Souza <lucas.souza@systax.com.br>
2022-09-15 00:25:34 -0300

    Adjustments to DDL Script

--- db/init-scripts/ddl.sql
+++ db/init-scripts/ddl.sql
@@ -1,7 +1,7 @@
 CREATE TABLE if not exists `user`(
     `id` bigint(20) NOT NULL AUTO_INCREMENT,
     `username` varchar(14) NOT NULL,
-    `date_joined` DATE not null default NOW(),
+    `date_joined` DATE not null default(CURRENT_DATE),
     PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -10,7 +10,7 @@ CREATE TABLE if not exists `post`(
     `repost_from_id` bigint(20), 
     `quote_from_id` bigint(20), 
     `user_id` bigint(20),
-    `datetime_creation` TIMESTAMP NOT NULL DEFAULT NOW(),
+    `datetime_creation` TIMESTAMP NOT NULL DEFAULT(CURRENT_TIMESTAMP),
     `text` varchar(777),
     PRIMARY KEY (id),
     FOREIGN KEY (repost_from_id) REFERENCES post(id),
