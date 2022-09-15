e69807236c989d6b82176eca0b1e7779c737c1aa
Lucas Pires de Souza <lucas.souza@systax.com.br>
2022-09-15 00:21:41 -0300

    Minor adjustment to DDL script

--- db/init-scripts/ddl.sql
+++ db/init-scripts/ddl.sql
@@ -1,7 +1,7 @@
 CREATE TABLE if not exists `user`(
     `id` bigint(20) NOT NULL AUTO_INCREMENT,
     `username` varchar(14) NOT NULL,
-    `date_joined` DATE not null default current_date(),
+    `date_joined` DATE not null default NOW(),
     PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
